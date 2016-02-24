df477af6dbcc668081b9fb1083f14bc4893c12a5
xlianChen <satikey@gmail.com>
2016-02-24 16:49:41 +1100

    BinaryGap

---
+++ BinaryGap/binaryGap.js
@@ -0,0 +1,23 @@
+
+// Find longest sequence of zeros in binary representation of an integer.
+
+function solution(N) {
+    
+    var binaryGap = 0;
+    var binaryRepresentation = [];
+    binaryRepresentation = N.toString(2);
+    
+    var firstFlag = 0
+    for(var i in binaryRepresentation) {
+        if(binaryRepresentation[i] == 0){
+            continue;
+        }else {
+            if(i - firstFlag > 0){
+                var tmp = i - firstFlag - 1;
+                binaryGap = tmp > binaryGap? tmp : binaryGap;
+                firstFlag = i;
+            }
+            
+        }
+    }
+    return binaryGap;
